fe053cff2e9762178084ce09cc3e325adb93e0ac
pheaktra1402 <pheaktrathorn474@gmail.com>
2026-09-16 10:37:14 +0700

    search and cart

--- react/src/components/Navbar.jsx
+++ react/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Logo from "../assets/logo.png";
+import { IoMdSearch } from "react-icons/io";
+import { FaCartShopping } from "react-icons/fa6";
 const Navbar = () => {
   return (
-    <div className="shadow-md bg-pink dark:bg-gray-900 dark:text-white">
+    
+    <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
       <div className="bg-primary/40 py-2">
         <div className="container flex justify-between items-center">
           <a href="#"
@@ -11,22 +14,34 @@ const Navbar = () => {
             <img src={Logo} alt="logo" className="w-20" />
             {/* KuyKun */}
           </a>
-       
-        <div className="group">
+          
+       {/* search */}
+        <div className="relative group hidden sm:block">
             <input type="text" placeholder="search"
             className="w-[200px] sm:w-[200px] 
             group-hover:w-[300px] 
             transition-all duration-300
             rounded-full border border-gray-300 px-2 py-1
-            focus:outline-none focus:border-1 
-            focus:border-primary"
+            focus:outline-none focus:border-2 
+            focus:border-pink-500"
+            
             />
+            <IoMdSearch className="text-gray-500 group-hover:text-primary 
+            absolute top-1/2 -translate-y-1/2 right-3"/>
         </div>
-        </div>
+        
+        {/* order button */}
+        <button 
+        onClick={()=> alert("Ordering not available yet")} className="bg-gradient-to-r 
+        from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group">
+          <span className="group-hover:block hidden transition-all duration-200">Order</span>
+          <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer"/>
+        </button>
+
       </div>
-      <div></div>
-      <div></div>
     </div>
+    </div>
+    
   );
 };
 
